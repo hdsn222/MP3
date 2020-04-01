8726d0fc67d945dad1f77f5de2c8ccb2abb057a3
jaredltrejo <jaredltrejo@gmail.com>
2020-04-01 17:23:54 -0400

    q1

--- jenkins/core/src/main/java/hudson/model/UpdateCenter.java
+++ jenkins/core/src/main/java/hudson/model/UpdateCenter.java
@@ -551,11 +551,28 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
                                     UpdateCenter.class.getName()+".xml"));
     }
 
+
+    public Map<String,Plugin> tryAddPluginToMap(Plugin plugin,Map<String,Plugin> pluginMap) {
+	 final Plugin existing = pluginMap.get(plugin.name);
+                if (existing == null) {
+                    pluginMap.put(plugin.name, plugin);
+                } else if (!existing.version.equals(plugin.version)) {
+                    // allow secondary update centers to publish different versions
+                    // TODO refactor to consolidate multiple versions of the same plugin within the one row
+                    final String altKey = plugin.name + ":" + plugin.version;
+                    if (!pluginMap.containsKey(altKey)) {
+                        pluginMap.put(altKey, plugin);
+                    }
+                }   
+	    return pluginMap;
+    }
     public List<Plugin> getAvailables() {
         Map<String,Plugin> pluginMap = new LinkedHashMap<String, Plugin>();
         for (UpdateSite site : sites) {
             for (Plugin plugin: site.getAvailables()) {
-                final Plugin existing = pluginMap.get(plugin.name);
+                tryAddPluginToMap(plugin,pluginMap);
+		/*
+		final Plugin existing = pluginMap.get(plugin.name);
                 if (existing == null) {
                     pluginMap.put(plugin.name, plugin);
                 } else if (!existing.version.equals(plugin.version)) {
@@ -566,6 +583,7 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
                         pluginMap.put(altKey, plugin);
                     }
                 }
+		*/
             }
         }
 
@@ -603,7 +621,9 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
         Map<String,Plugin> pluginMap = new LinkedHashMap<String, Plugin>();
         for (UpdateSite site : sites) {
             for (Plugin plugin: site.getUpdates()) {
-                final Plugin existing = pluginMap.get(plugin.name);
+                tryAddPluginToMap(plugin,pluginMap);
+		/*
+		final Plugin existing = pluginMap.get(plugin.name);
                 if (existing == null) {
                     pluginMap.put(plugin.name, plugin);
                 } else if (!existing.version.equals(plugin.version)) {
@@ -614,6 +634,7 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
                         pluginMap.put(altKey, plugin);
                     }
                 }
+		*/
             }
         }
 
