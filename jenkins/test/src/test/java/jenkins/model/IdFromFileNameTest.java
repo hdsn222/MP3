77655b1eac31c582626b67b5b86b8e163d35571e
jaredltrejo <jaredltrejo@gmail.com>
2020-04-08 20:25:31 -0400

    test file for q4

--- jenkins/test/src/test/java/jenkins/model/IdFromFileNameTest.java
+++ jenkins/test/src/test/java/jenkins/model/IdFromFileNameTest.java
@@ -1,6 +1,7 @@
 package jenkins.model;
 
 import jenkins.model.IdStrategy;
+import hudson.model.User;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -8,13 +9,15 @@ public class IdFromFileNameTest {
 	//@Rule public JenkinsRule j = new JenkinsRule();
 
 	@Test public void test1() {
-		
+		//User user = User.get("john smith");
+		//User user2 = User.get("John Smith");
 		IdStrategy ids = new IdStrategy.CaseSensitive();
-		String filename = "$123";
-		assertEquals("",ids.idFromFilename(filename));
+		String filename = "$0031";
+		assertEquals("1",ids.idFromFilename(filename));
+		//assertEquals(user.getId(),User.idStrategy().idFromFilename(User.idStrategy().filenameOf(user.getId())));
 	}
 
-	@Test public void test2() {
+	@Test public void testBreak() {
 		IdStrategy ids = new IdStrategy.CaseSensitive();
 		String filename = "$12";
 		assertEquals("",ids.idFromFilename(filename));
